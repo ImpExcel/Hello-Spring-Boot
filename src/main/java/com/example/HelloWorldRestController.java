40c145b413de7bba9f695f83df6c262a3b24d735
ImpExcel <czjpercy@gmail.com>
2017-05-28 22:16:27 -0700

    Upload project

---
+++ src/main/java/com/example/HelloWorldRestController.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldRestController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String greeting() {
+        return "Hello Spring Boot";
+    }
+
+
+    @RequestMapping(value = "/{version}", method = RequestMethod.GET)
+    public String greeting(@PathVariable String version) {
+        return "Hello Spring Boot " + version;
+    }
+
+}
